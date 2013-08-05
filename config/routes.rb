9ad2ef91011599dbba9ad1aa13b723ab8690296b
Víctor <ortega_1989@hotmail.com>
2013-08-06 00:18:24 +0200

    pruebas hw3

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,6 @@ Rottenpotatoes::Application.routes.draw do
   resources :movies
   # map '/' to be a redirect to '/movies'
   root :to => redirect('/movies')
+  #Route that posts 'Search TMDb' form
+  post 'movies/search_tmdb'
 end
